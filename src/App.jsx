57df0f7f1985539e61f06c41ed2b4529f1907c8e
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-09-30 12:28:38 +0200

    Extract Image Gallery to its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
-import { useEffect, useState, React } from 'react';
+import { useState, React } from 'react';
 import './styles/App.css';
-import fetchImages from './utils/fetchImages';
+import ImageGallery from './components/ImageGallery';
 
 function App() {
   const [allImages, setImages] = useState([]);
 
-  useEffect(() => {
-    async function prepareImages() {
-      setImages(await fetchImages());
-    }
-    prepareImages();
-  }, []);
-
-  // Source: https://bobbyhadz.com/blog/javascript-get-multiple-random-elements-from-array
-  // Returns 5 random images
-  function pickRandomImages() {
-    const shuffled = [...allImages].sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, 5);
-  }
-
-  const randomImages = pickRandomImages();
-  console.log(pickRandomImages());
-
-  return (
-    <div className="image-carousel">
-      {randomImages.map((image) => (
-        <div key={image.id} className="image-wrapper">
-          <img src={image.url} alt="semi-randomly chosen" />
-          <figcaption>{`Credit: ${image.author}`}</figcaption>
-        </div>
-      ))}
-    </div>
-  );
+  return <ImageGallery allImages={allImages} setImages={setImages} />;
 }
 
 export default App;
