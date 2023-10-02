0f23d7eb5fecbc201184482a5e0be7d5f9afb0fc
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-10-02 11:45:31 +0200

    Remove pexels npm package
     - Pexels API wrapper was using a dependency (isomorphic-fetch) that
       uses ES5 imports, which could not be bundled using Vite. Instead of
       the wrapper, fetch() is now used.
     - Uninstall unused npm packages

--- src/utils/fetchImages.js
+++ src/utils/fetchImages.js
@@ -1,10 +1,17 @@
-import { createClient } from 'pexels';
+// import { createClient } from 'pexels';
 
 export default async function fetchImages() {
   const APIkey = import.meta.env.VITE_PEXELS;
-  const client = createClient(APIkey);
+  const query = 'trains';
 
-  const query = 'train';
+  const imageRequest = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`,
+    {
+      headers: {
+        Authorization: APIkey,
+      },
+    },
+  ).then((response) => response.json());
 
   // API mock
   /*
@@ -455,8 +462,6 @@ export default async function fetchImages() {
   };
 
   */
-
-  const imageRequest = await client.photos.search({ query, per_page: 15 });
 
   const images = [...imageRequest.photos].map((image) => ({
     author: image.photographer,
